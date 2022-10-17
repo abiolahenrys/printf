358176a23481ba98057796430686b0121c60a02c
Mac <mac@Mars-MacBook-Pro.local>
2022-10-17 17:15:59 +0100

    third file

--- _printf.c
+++ _printf.c
@@ -1,13 +1,8 @@
 #include "main.h"
-#include <limits.h>
-#include <stdio.h>
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
  */
 int _printf(const char *format, ...)
 {
